205aa75df4a358f160d4fcf9b82b770826bca0ee
Ninxxx1 <116868011+Ninxxx1@users.noreply.github.com>
2024-05-13 02:09:58 +0100

    Kreiran springboot projekat u IntelliJ

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # bsep-ra-2024-kt2-tim-32
+
+Lidija Djurdjic RA 116/2020
+Olga Ponjevic RA /2020
+Katarina Vurdelja RA 197/2020
+
+Funckionalnosti za drugu kontrolnu tacku:
+3.1
+3.2 
+3.3
+3.4
+3.7
+3.8
+3.9
+3.10
+3.11
+3.12
+3.13
+3.14
---
+++ bezbednost-tim-32/src/test/java/com/bsep/bezbednosttim32/BezbednostTim32ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsep.bezbednosttim32;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezbednostTim32ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
