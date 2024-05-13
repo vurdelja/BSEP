35ce63129d7c12add6df128949d4a18f45eec49e
Ninxxx1 <116868011+Ninxxx1@users.noreply.github.com>
2024-05-13 20:14:45 +0100

    Added UserDetails for User class

---
+++ bezbednost-tim-32/src/main/java/com/bsep/bezbednosttim32/model/Role.java
@@ -0,0 +1,8 @@
+package com.bsep.bezbednosttim32.model;
+
+public enum Role {
+    USER,
+    EMPLOYEE,
+    ADMIN,
+    UNAUTHENTICATED_USER
+}
--- bezbednost-tim-32/src/main/java/com/bsep/bezbednosttim32/model/User.java
+++ bezbednost-tim-32/src/main/java/com/bsep/bezbednosttim32/model/User.java
@@ -1,16 +1,27 @@
 package com.bsep.bezbednosttim32.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
-    private long id;
+    @GeneratedValue
+    private Integer id;
 
     private String email;
     private String password;
@@ -23,6 +34,44 @@ public class User {
     private UserType userType;
     private String companyName; // For legal entities
     private String PIB; // PIB for legal entities
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
 
 enum UserType {
