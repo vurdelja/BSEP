bc9b6a30d73204b1b05bfdf347c4c405fc494dfc
Ninxxx1 <116868011+Ninxxx1@users.noreply.github.com>
2024-06-14 01:25:40 +0200

    Popravljeno slanje mejla

--- bezbednost-tim-32/src/main/java/com/bsep/bezbednosttim32/config/MailConfig.java
+++ bezbednost-tim-32/src/main/java/com/bsep/bezbednosttim32/config/MailConfig.java
@@ -23,6 +23,7 @@ public class MailConfig {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "*");
         props.put("mail.debug", "true");
 
         return mailSender;
